352d36644ca7006d786f299de762ec018ece7b7e
Nevin Jojo <nevinjojo98@gmail.com>
2019-03-14 16:11:49 +1300

    top10balance ready to be run on framework

--- scripts/aix/top10Balances.sql
+++ scripts/aix/top10Balances.sql
@@ -2,31 +2,30 @@
 -- Top 10 balances per month (excluding systems)
 -- Author: Nevin Jojo
 ----------------------------------------------------------------------
-WITH Cte AS
-(
-    SELECT
-    *,
-    row_number() over(PARTITION BY month ORDER BY month) as rowNum
+WITH 
+    topBalances AS
+    (
+        SELECT
+            *,
+            row_number() over(PARTITION BY month ORDER BY month) as rowNum
+        FROM
+            (SELECT
+                (to_char(changed, 'yyyy-MM'))as month,
+                created,
+                sum(total) as total
+            FROM
+                balance
+            GROUP BY
+                month, 
+                created
+            ORDER BY
+                month,
+                total DESC
+            ) b
+    )
+SELECT
+    * 
 FROM
-    (SELECT
-        (to_char(changed, 'yyyy-MM'))as month,
-        created,
-        sum(total) as total
---        row_number() over(PARTITION BY (to_char(changed, 'yyyy-MM')) ORDER BY (to_char(changed, 'yyyy-MM'))) as row
-    FROM
-        balance
-    GROUP BY
-        month, 
-        created
-    ORDER BY
-        --DATE_PART('year', changed),
-        --DATE_PART('month', changed), 
-        month,
-        total DESC
-    ) b
-)
-select * 
-FROM
-    Cte
+    topBalances
 WHERE
     rowNum <= 10;
